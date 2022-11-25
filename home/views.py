48909d6bd03abd72e075a76b47cf46cee89e9534
Seymen Can Aydogan <b210109063@subu.edu.tr>
2022-11-26 02:13:33 +0300

    Setting data tablosu oluşturuldu ve veri çekildi.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from home.models import Setting
+
 def index(request):
-    return render(request,'index.html')
+    setting=Setting.objects.get(pk=1)
+    context={'setting':setting}
+    return render(request,'index.html',context)
